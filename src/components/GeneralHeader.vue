d1ed5054a2e08748b3a89970bf7d3616f1c87d62
alexchamp <sasha.g27204@gmail.com>
2023-02-16 19:51:22 +0300

    fix: path

--- src/components/GeneralHeader.vue
+++ src/components/GeneralHeader.vue
@@ -8,7 +8,7 @@
                 <div class="links">
                     <router-link to="/" class="btn">Главная</router-link>
                     <router-link to="/pallet" class="btn">Поддоны</router-link>
-                    <a href="http://192.168.31.15:8080/#about-us" class="btn">О нас</a>
+                    <a href="http://pallet-rnd.ru/#about-us" class="btn">О нас</a>
                 </div>
                 <div class="contacts">
                     <span>+7-(918)-598-99-22</span>
